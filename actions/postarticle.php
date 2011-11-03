8eec179692eba27974a1c6af4e51198260878b05
Lance Bradbury <lance.bradbury@colorado.edu>
2011-11-02 19:39:02 -0600

    Added the sql

--- actions/postarticle.php
+++ actions/postarticle.php
@@ -6,6 +6,19 @@ class page{
 	{
 		$this -> title = "New Post";
 	}
+	private function post($p, $t)
+	{
+		global $sql;
+		global $user;
+		$post = $sql -> san($p);
+		$title = $sql -> san($t);
+		$q = "INSERT INTO articles (article_title, poster_id, public) VALUES ('".$title . "', ".$user['user_id'].", 1)";
+		$sql -> query($q);
+		$aid = $sql -> query("SELECT article_id FROM articles WHERE poster_id = ".$user['user_id']." and article_title = '".$title."'");
+		$raid = $sql -> fetchAssoc($aid);
+		$qu= "INSERT INTO posts (poster_id, article_id, approved, post, posted_on) VALUES (".$user['user_id'].", ".$raid['article_id'].", 1, '".$post."', NOW())";
+		$sql -> query($qu);
+	}
 	public function main()
 	{
 		global $user;
@@ -16,6 +29,10 @@ class page{
 			if($user['can_post_article'])
 			{
 				$this -> content = $tmp -> get("postNewArticle");
+			}
+			else
+			{
+				$this -> content = "You do not have permission to post!";
 			}
 		}
 		else
--- template/postNewArticle.php
+++ template/postNewArticle.php
@@ -1,6 +1,8 @@
 <form action="[[url]]?act=postarticle" method="post">
-	<textarea name = "post" cols = "60" rows = "15">
-	Enter Post Here
-	</textarea><br>
+	<input type = "text" name = "title" placeholder = "Post Title">
+	<br>
+	<textarea name = "post" cols = "60" rows = "15" placeholder = "Enter Post Here">
+	</textarea>
+	<br>
 	<input type = "submit" value = "Post" />
 </form>
