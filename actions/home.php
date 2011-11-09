e23a8b785c1eb04de80b8b62c5a9686a2c8c636a
James Blades <troomps01@gmail.com>
2011-11-09 10:32:10 -0800

    Modifying home.

--- actions/home.php
+++ actions/home.php
@@ -7,8 +7,24 @@ class Page{
 		$this->title = "Home";
 	}
 	public function main(){
+		global $sql;
+		$page = 0;
+		if(isset($_GET['file']) && is_numeric($_GET['file'])){
+			$page = $_GET['file'];
+		}
 		$tmp = new Template();
-		$this->content = $tmp->get("home");
+		$query = "SELECT articles.article_id, articles.article_title, articles.poster_id FROM articles ORDER BY article_id DESC LIMIT " . ($page * 10) . ",10";
+		$articles = $sql->query($query);
+		$content = "";
+		while($article = $sql->fetchAssoc($articles)){
+			$q = "SELECT posts.post, posts.posted_on, users.username FROM posts, users WHERE posts.article_id = " . $article['article_id'] . " AND users.user_id = posts.poster_id ORDER BY posted_on ASC LIMIT 1";
+			$results = $sql->query($q);
+			$row = $sql->fetchAssoc($results);
+			if(!$row){
+				continue;
+			}
+			$content .= Replace::on($tmp->get());
+		}
 	}
 }
 
