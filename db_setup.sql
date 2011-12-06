1b78f90ee11e52a6be77a463614412fff9395e53
James Blades <troomps01@gmail.com>
2011-12-06 09:57:05 -0800

    Updated sql dump

--- db_setup.sql
+++ db_setup.sql
@@ -28,7 +28,7 @@ CREATE TABLE `articles` (
   `public` smallint(1) NOT NULL DEFAULT '0',
   `poster_id` bigint(10) NOT NULL,
   PRIMARY KEY (`article_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +37,7 @@ CREATE TABLE `articles` (
 
 LOCK TABLES `articles` WRITE;
 /*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` VALUES (1,'A weekend of Skyrim',1,1),(2,'Test',1,1);
 /*!40000 ALTER TABLE `articles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,9 +84,9 @@ CREATE TABLE `posts` (
   `article_id` bigint(10) NOT NULL,
   `approved` smallint(1) NOT NULL DEFAULT '0',
   `post` text NOT NULL,
-  `posted_on` date DEFAULT NULL,
+  `posted_on` datetime NOT NULL,
   PRIMARY KEY (`post_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,6 +95,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,1,1,1,'Well, as many of you probably have figured out by this point in time, Skyrim has come out! Sadly, I have spent most of the weekend playing it, and probably clocked 30+ hours playing or watching others play the game. \r\n\r\nIn short, Skyrim is an amazing way to waste away time.\r\n\r\nNow, I do have some theories about how the game works. It appears to level enemies not as you level, but on a tiered system. From what I gather enemies get more difficult every time you reach a multiple of 10. For instance, I didn\'t see any Blood Dragons before I got to level 10, but since I have I haven\'t seen any normal dragons. Perhaps I am just unlucky when it comes to finding baddies, but Blood Dragons also seem to do a considerably higher amount of damage. Just something to keep in mind when you are playing.	','2011-11-14 00:00:00'),(2,1,2,1,'This is a nice little test\r\n\r\n\r\nasd	\r\n\r\n\r\nFroggy and the other things.','2011-11-16 00:00:00');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +112,7 @@ CREATE TABLE `sessions` (
   `hash` varchar(32) NOT NULL,
   `created_on` date DEFAULT NULL,
   PRIMARY KEY (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,6 +121,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `sessions` VALUES (9,1,'8413efd7bd59ef8114dbb6b6ec6241f7','2011-11-09'),(2,1,'080c04b87eb49a0d67acf02525583815','2011-11-04'),(3,1,'080c04b87eb49a0d67acf02525583815','2011-11-04'),(10,1,'db283c49c7314f0d74117d4768860522','2011-11-14'),(12,1,'b7e408f49240f27b7a7de6894a020bfd','2011-11-16');
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,7 +141,7 @@ CREATE TABLE `users` (
   `joined_on` date DEFAULT NULL,
   `last_login` date DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,6 +150,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,1,'James','c96dd568316deb9d8c7dec73b4c27cbb','james.blades@colorado.edu',NULL,NULL),(2,1,'Myusername','5f4dcc3b5aa765d61d8327deb882cf99','fake.email@gmail.com',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -159,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-11-02 14:07:42
+-- Dump completed on 2011-12-06  9:56:46
